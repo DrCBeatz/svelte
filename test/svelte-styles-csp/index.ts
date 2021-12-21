b14926d2df254bb4645f2b6bd81abb0672874c94
drcbeatz <brynecarruthers@gmail.com>
2021-12-21 01:22:44 -0500

    moved location of browser.close() in tests

--- test/svelte-styles-csp/index.ts
+++ test/svelte-styles-csp/index.ts
@@ -27,11 +27,11 @@ describe('svelte-style-csp', async () => {
           );
         });
         const linkList = await linkTitles.jsonValue();
-
+        
+        await browser.close();
         for (const item of linkList) {
           if (item == 'svelte-stylesheet') linkTitle = item;
         }
-        await browser.close();
         assert.equal(linkTitle, 'svelte-stylesheet');
       } catch (err) {
         throw new Error(err);
@@ -61,35 +61,6 @@ describe('svelte-style-csp', async () => {
   });
 
   describe('test transitions without svelte-styles-csp & strict CSP', async () => {
-    it('svelte-stylesheet not found', async () => {
-      const absolutePath = path.join(
-        __dirname,
-        'svelte-test-transitions-no-styles-csp/index.html'
-      );
-      const browser = await puppeteer.launch({ headless: headless_browser });
-      const page = await browser.newPage();
-      try {
-        await page.goto('file://' + absolutePath);
-        let linkTitle: string;
-        const linkTitles = await page.evaluateHandle(() => {
-          return Array.from(document.getElementsByTagName('link')).map(
-            (a) => a.title
-          );
-        });
-        const linkList = await linkTitles.jsonValue();
-
-        for (const item of linkList) {
-          if (item == 'svelte-stylesheet') linkTitle = item;
-        }
-        await browser.close();
-        if (linkTitle) {
-          console.log(linkTitle);
-        }
-      } catch (err) {
-        // svelte-stylesheet is not present so throws error
-        assert.throws(err);
-      }
-    });
     it('transitions fail with strict CSP and no style-src: unsafe-inline', async () => {
       const absolutePath = path.join(
         __dirname,
@@ -104,11 +75,11 @@ describe('svelte-style-csp', async () => {
           await page.waitFor(1000);
         }
         await page.click('input');
-        await browser.close();
       } catch (err) {
         // Transitions should fail with strict CSP
         assert.throws(err);
       }
+      await browser.close();
     });
   });
 });
